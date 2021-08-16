d1202344764a41ea4358aeff74963780cfa22b10
goksenpasli <gpasli@gmail.com>
2021-08-16 21:33:28 +0300

    Merge branch 'master' of https://github.com/goksenpasli/GpSinema

--- Sinema/ViewModel/ReportViewModel.cs
+++ Sinema/ViewModel/ReportViewModel.cs
@@ -1,17 +1,22 @@
 ﻿using Extensions;
 using Sinema.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Windows.Data;
 using System.Windows.Input;
+using System.Windows.Media;
 using System.Xml.Linq;
 
 namespace Sinema.ViewModel
 {
-    public class ReportViewModel
+    public class ReportViewModel : InpcBase
     {
+        private ObservableCollection<GraphControl.Chart> graphData = new();
+
         public ReportViewModel()
         {
             KişiSiparişListesiRaporla = new RelayCommand<object>(parameter =>
@@ -35,6 +40,15 @@ namespace Sinema.ViewModel
                 }
             }, parameter => parameter is not null);
 
+            SalonFilmTutarRaporla = new RelayCommand<object>(parameter =>
+            {
+                const string filepath = @"\Raporlar\SalonFilmlerTutar.xlsx";
+                if (File.Exists(ExeFolder + filepath))
+                {
+                    FilmTutarlarınıAl().CreateReport(ExeFolder + filepath);
+                }
+            }, parameter => true);
+
             FilmListesiRaporla = new RelayCommand<object>(parameter =>
             {
                 const string filepath = @"\Raporlar\Filmler.xlsx";
@@ -46,14 +60,53 @@ namespace Sinema.ViewModel
                     list = null;
                 }
             }, parameter => true);
+
+            foreach (var item in FilmTutarlarınıAl())
+            {
+                var rand = new Random(Guid.NewGuid().GetHashCode());
+                var brush = new SolidColorBrush(Color.FromRgb((byte)rand.Next(0, 256), (byte)rand.Next(0, 256), (byte)rand.Next(0, 256)));
+                GraphData.Add(new GraphControl.Chart() { Description = item.Adı, ChartValue = item.Fiyat, ChartBrush = brush});
+            }
         }
 
         public ICommand FilmListesiRaporla { get; }
+
+        public ObservableCollection<GraphControl.Chart> GraphData
+        {
+            get => graphData;
+
+            set
+            {
+                if (graphData != value)
+                {
+                    graphData = value;
+                    OnPropertyChanged(nameof(GraphData));
+                }
+            }
+        }
 
         public ICommand KişiSiparişListesiRaporla { get; }
 
         public ICommand SalonDurumuRaporla { get; }
 
+        public ICommand SalonFilmTutarRaporla { get; }
+
         private string ExeFolder { get; } = Path.GetDirectoryName(Process.GetCurrentProcess().MainModule.FileName);
+
+        private static IEnumerable<ReportData> FilmTutarlarınıAl()
+        {
+            var document = XDocument.Load(MainWindowViewModel.xmldatapath);
+            return document.Descendants("Müşteri").Select(z => new ReportData()
+            {
+                Adı = document.Descendants("Film").FirstOrDefault(d => (int)d.Attribute("Id") == (int)z.Attribute("FilmId"))?.Attribute("Adı").Value,
+                FilmSaati = (DateTime)document.Descendants("Film").FirstOrDefault(d => (int)d.Attribute("Id") == (int)z.Attribute("FilmId"))?.Attribute("FilmSaati"),
+                Fiyat = (double)z.Attribute("BiletFiyat")
+            });
+        }
+    }
+
+    internal class ReportData : Film
+    {
+        public double Fiyat { get; set; }
     }
 }
